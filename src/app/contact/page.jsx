fb63a213371aef465857f226b0d86973be4a0a7c
pankajkoree <pankajkori2057@gmail.com>
2025-07-10 13:57:08 +0545

    contact page

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,8 @@
+export default function ContactPage() {
+  return (
+    <>
+      <h2 className="text-3xl font-bold mb-4">Contact Us</h2>
+      <p>Get in touch with us via phone, email, or WhatsApp.</p>
+    </>
+  );
+}
